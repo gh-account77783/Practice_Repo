894eca2fa1e463f9f2c76938da0088d46517e3ab
Gaurav <gaurav28.24@kgpian.iitkgp.ac.in>
2026-05-20 17:17:21 +0530

    ending line frontend.py

--- front-end.py
+++ front-end.py
@@ -5,3 +5,5 @@ for i in range(5):
 
 ##This is front end code for a simple application. It demonstrates basic printing and looping in Python. The code can be expanded to include more complex functionality as needed.
 print("Front-end code executed successfully!")
+
+print("This is a simple front-end application in Python.")
